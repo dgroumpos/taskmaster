dd30e8df573c543b3c9e3a214b3fbff1fab6a878
Dimitris Groumpos <dngroumpos@hotmail.com>
2023-10-21 23:02:47 +0300

    Style the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { GiMagicBroom } from "react-icons/gi";
+import classNames from "classnames";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     { href: "/", label: "Dashboard" },
     { href: "/tasks", label: "Tasks" },
@@ -15,9 +21,13 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map((link) => (
           <Link
+            className={classNames({
+              "text-amber-950": link.href === currentPath,
+              "text-amber-600": link.href !== currentPath,
+              "hover:text-amber-800 transition-colors": true,
+            })}
             href={link.href}
             key={link.href}
-            className="text-amber-600 hover:text-amber-950 transition-colors"
           >
             {link.label}
           </Link>
